c31900eab384ad767ff1eeb96f60c4867a4f5e10
Andrés Gorea Olari <andres.g.principal@gmail.com>
2026-05-20 10:18:43 +0200

    backend agregado

---
+++ backend/demo/src/test/java/com/ntt/libreria/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntt.libreria.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
