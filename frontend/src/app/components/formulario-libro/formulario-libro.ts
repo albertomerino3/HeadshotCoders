b5f3488e21ac36fe846d7ce111d6b496b736a55f
Andrés Gorea Olari <andres.g.principal@gmail.com>
2026-05-20 10:29:53 +0200

    frontend agregado

---
+++ frontend/src/app/components/formulario-libro/formulario-libro.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-libro',
+  imports: [],
+  templateUrl: './formulario-libro.html',
+  styleUrl: './formulario-libro.css',
+})
+export class FormularioLibro {}
---
+++ frontend/src/app/components/lista-libros/lista-libros.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-libros',
+  imports: [],
+  templateUrl: './lista-libros.html',
+  styleUrl: './lista-libros.css',
+})
+export class ListaLibros {}
